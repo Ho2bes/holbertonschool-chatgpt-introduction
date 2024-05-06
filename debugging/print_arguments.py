eec5da3a482432a90343415300d9518179362112
Ho2bes <8696@holbertonstudents.com>
2024-05-06 11:18:53 +0200

    modification code

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1
-    return result
-
-if len(sys.argv) != 2:
-    print("Usage: ./print_factorial.py <number>")
-    sys.exit(1)
-
-try:
-    num = int(sys.argv[1])
-    print(factorial(num))
-except ValueError:
-    print("Error: Argument must be an integer.")
-    sys.exit(1)
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
