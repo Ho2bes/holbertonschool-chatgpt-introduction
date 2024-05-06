ea1bcc38f44819ea913d4e94201d78dde2b65010
Ho2bes <8696@holbertonstudents.com>
2024-05-06 11:18:07 +0200

    modification code

--- debugging/mines.py
+++ debugging/mines.py
@@ -9,11 +9,11 @@ class Minesweeper:
     def __init__(self, width=10, height=10, mines=10):
         self.width = width
         self.height = height
-        self.total_cells = width * height
-        self.mines = set(random.sample(range(self.total_cells), mines))
+        self.mines = set()
+        while len(self.mines) < mines:
+            self.mines.add(random.randint(0, width * height - 1))
         self.field = [[' ' for _ in range(width)] for _ in range(height)]
         self.revealed = [[False for _ in range(width)] for _ in range(height)]
-        self.remaining_cells = self.total_cells - mines
 
     def print_board(self, reveal=False):
         clear_screen()
@@ -44,10 +44,7 @@ class Minesweeper:
     def reveal(self, x, y):
         if (y * self.width + x) in self.mines:
             return False
-        if self.revealed[y][x]:
-            return True
         self.revealed[y][x] = True
-        self.remaining_cells -= 1
         if self.count_mines_nearby(x, y) == 0:
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
@@ -56,22 +53,22 @@ class Minesweeper:
                         self.reveal(nx, ny)
         return True
 
-    def check_win(self):
-        return self.remaining_cells == 0
-
     def play(self):
         while True:
             self.print_board()
             try:
                 x = int(input("Enter x coordinate: "))
                 y = int(input("Enter y coordinate: "))
+                if not (0 <= x < self.width and 0 <= y < self.height):
+                    print("Coordinates out of bounds. Please try again.")
+                    continue
                 if not self.reveal(x, y):
                     self.print_board(reveal=True)
                     print("Game Over! You hit a mine.")
                     break
-                if self.check_win():
+                elif all(self.revealed[i][j] or (i * self.width + j) in self.mines for i in range(self.height) for j in range(self.width)):
                     self.print_board(reveal=True)
-                    print("Congratulations! You won the game!")
+                    print("Congratulations! You've won the game.")
                     break
             except ValueError:
                 print("Invalid input. Please enter numbers only.")
